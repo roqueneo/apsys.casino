396f5ca426072510ddadfcbcd1ac81d00d5e1523
Alonso Roque A <alonso.roque@apsys.mx>
2021-09-03 19:42:33 -0500

    Initialize repository

---
+++ apsys.casino.domain.testing/CardTests.cs
@@ -0,0 +1,65 @@
+﻿using apsys.casino.domain.Shared;
+using NUnit.Framework;
+
+namespace apsys.casino.domain.testing
+{
+    class CardTests
+    {
+        [TestCase(SuitConstats.Diamonds)]
+        [TestCase(SuitConstats.Spades)]
+        [TestCase(SuitConstats.Clubs)]
+        [TestCase(SuitConstats.Hearts)]
+        public void IsValid_ValidSuit_ReturnTrue(string suit)
+        {
+            // Arrange
+            Card card = new Card();
+            card.SetMockData();
+            card.Suit = suit;
+            // Act
+            bool result = card.IsValid();
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("COINS")]
+        [TestCase("1")]
+        public void IsValid_InvalidSuit_ReturnFalse(string suit)
+        {
+            // Arrange
+            Card card = new Card();
+            card.SetMockData();
+            card.Suit = suit;
+            // Act
+            bool result = card.IsValid();
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        [TestCase("6")]
+        [TestCase("7")]
+        [TestCase("8")]
+        [TestCase("9")]
+        [TestCase("10")]
+        [TestCase("J")]
+        [TestCase("Q")]
+        [TestCase("K")]
+        public void IsValid_ValidValue_ReturnTrue(string value)
+        {
+            // Arrange
+            Card card = new Card();
+            card.SetMockData();
+            card.Value = value;
+            // Act
+            bool result = card.IsValid();
+            // Assert 
+            Assert.IsTrue(result);           
+        }
+    }
+}
---
+++ apsys.casino.domain/Card.cs
@@ -0,0 +1,30 @@
+﻿using apsys.casino.domain.Shared;
+using System.Linq;
+
+namespace apsys.casino.domain
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+
+        public string Value { get; set; }
+
+        public bool IsValid()
+        {
+            string[] validValues = new string[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+            if (
+                (Suit == SuitConstats.Diamonds || Suit == SuitConstats.Spades || Suit == SuitConstats.Clubs || Suit == SuitConstats.Hearts)
+                && (validValues.Contains(Value))
+            )
+                return true;
+            else
+                return false;
+        }
+
+        public void SetMockData()
+        {
+            this.Suit = SuitConstats.Diamonds;
+            this.Value = "1";
+        }
+    }
+}
